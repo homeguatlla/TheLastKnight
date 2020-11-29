1d6f3c7511d48625b0646c973924a5eedf285c63
david.guerrero <dguerrero_bcn@hotmail.com>
2020-11-29 23:14:17 +0100

    Added a ground circle on the NPC in debug mode to distinguish the npc from which the data is displayed

---
+++ Source/TheLastKnight/Components/GroundCircleComponent.cpp
@@ -0,0 +1,64 @@
+#include "GroundCircleComponent.h"
+#include <Kismet/GameplayStatics.h>
+#include <GameFramework/Character.h>
+#include <TheLastKnight/TheLastKnightGameMode.h>
+#include <TheLastKnight/Agents/AI/NPCAIController.h>
+
+
+
+void UGroundCircleComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BindToDelegate();
+
+	this->SetVisibility(false);
+	mAgentName = GetAgentName();
+}
+
+void UGroundCircleComponent::BindToDelegate()
+{
+	auto gameMode = Cast<ATheLastKnightGameMode>(UGameplayStatics::GetGameMode(this->GetWorld()));
+	if (gameMode)
+	{
+		auto eventDispatcher = gameMode->GetEventDispatcher();
+
+		eventDispatcher->OnEnableDebugMode.AddDynamic(this, &UGroundCircleComponent::OnEnableDebugMode);
+		eventDispatcher->OnNextNPC.AddDynamic(this, &UGroundCircleComponent::OnNextNPC);
+		eventDispatcher->OnPreviousNPC.AddDynamic(this, &UGroundCircleComponent::OnPreviousNPC);
+	}
+}
+
+void UGroundCircleComponent::OnEnableDebugMode(bool enable)
+{
+	if(!enable)
+	{
+		this->SetVisibility(false);
+	}
+}
+
+void UGroundCircleComponent::OnNextNPC(const FString& name)
+{
+	this->SetVisibility(mAgentName == name);
+}
+
+void UGroundCircleComponent::OnPreviousNPC(const FString& name)
+{
+	this->SetVisibility(mAgentName == name);
+}
+
+FString UGroundCircleComponent::GetAgentName() const
+{
+	FString name = FString("");
+
+	auto actor = Cast<ACharacter>(GetOwner());
+	if (actor != nullptr)
+	{
+		auto npcAIController = Cast<ANPCAIController>(actor->GetController());
+		if (npcAIController != nullptr)
+		{
+			name = npcAIController->GetAgentName();
+		}
+	}
+	return name;
+}
